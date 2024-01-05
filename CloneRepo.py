d3e04e58aff19ebea98043bc179175c5d7acb6e4
prg-maker <df3296709@gmail.com>
2024-01-05 20:32:55 -0300

    fist commit

---
+++ CloneRepo.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+
+
+
+
+def functionComand(comando):
+    return subprocess.check_output(comando , shell= True)
+
+
+print("Nome do repositorio: ");
+repository = input();
+
+comando = f'git clone {repository} '
+saida = functionComand(comando)
+
+nome = repository.split("/")
+
+
+os.chdir(f"/home/daniel/projetos/command/{nome[-1]}");
+comando = "npm i "
+saida = functionComand(comando)

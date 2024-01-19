eaa5e0aa884beb12f1d59ed54376e2535b4e9929
prg-maker <df3296709@gmail.com>
2024-01-19 11:00:08 -0300

    add code .

--- createNewProjectReactTs.py
+++ createNewProjectReactTs.py
@@ -6,12 +6,18 @@ def functionComand(comando):
 
 
 def creteNewProjecteReact():
-
+   
+    os.chdir("/home/daniel/projetos/daniel");
     nome = input("Nome da aplicaçao: ")
     
     comando = f"npm create vite@latest {nome} -- --template react-ts"
     functionComand(comando)
 
-    os.chdir(f"/home/daniel/projetos/command/{nome}");
+    os.chdir(f"/home/daniel/projetos/daniel/{nome}");
     comando = "npm i "
     functionComand(comando)
+
+    functionComand("code .")
+    print(f"Projeto criado {nome}.")
+   
+

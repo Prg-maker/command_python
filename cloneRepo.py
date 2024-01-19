b97f85734f37396b2addfd040568ac15c52ea282
prg-maker <df3296709@gmail.com>
2024-01-19 11:30:49 -0300

    .

--- cloneRepo.py
+++ cloneRepo.py
@@ -5,23 +5,28 @@ def functionComand(comando):
     return subprocess.check_output(comando , shell= True)
 
 def cloneRepo():
+    os.chdir(f"/home/daniel/projetos/daniel");
+
     repository = input("Nome do repositorio: ");
 
     comando = f'git clone {repository} '
-    saida = functionComand(comando)
+    functionComand(comando)
 
     nome = repository.split("/")
     nome  = nome[-1].split(".")
 
-    os.chdir(f"/home/daniel/projetos/command/{nome[0]}");
+    os.chdir(f"/home/daniel/projetos/daniel/{nome[0]}");
 
 
     arquivos=[]
 
-    for nome_arquivos in os.listdir(f"/home/daniel/projetos/command/{nome[0]}"):
+    for nome_arquivos in os.listdir(f"/home/daniel/projetos/daniel/{nome[0]}"):
         if(nome_arquivos.endswith("js")):
             arquivos.append(nome_arquivos)    
 
     if(len(arquivos) != 0):
         comando = "npm i "
-        saida = functionComand(comando)
+        functionComand(comando)
+    
+    functionComand("code .")
+    
